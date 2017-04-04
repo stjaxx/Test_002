9a9650888994e16a2b994557d48f8a33adce3b46
Ivan <Ivan@DESKTOP-SA9PJ6H>
2017-04-04 20:14:22 +0200

    Added Main Class
    added one class
    
    Change-Id: I7777bbbca5178b3cca40dca1891d4d8ff0dff3dc
    Signed-off-by: Ivan <Ivan@DESKTOP-SA9PJ6H>

---
+++ Test_002/src/hr/dalmatia/split/blatine/MainClass.java
@@ -0,0 +1,11 @@
+package hr.dalmatia.split.blatine;
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Play games");
+		System.out.println("Neæeš razbojnièe");
+
+	}
+
+}
